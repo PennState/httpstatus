fbe38af0d870f18f0cd11d067fa44b2051ce3cdc
Steve Moyer <smoyer@psu.edu>
2020-02-28 14:42:18 -0500

    Updates comment format (per convention)

--- pkg/httpstatus/pkg.go
+++ pkg/httpstatus/pkg.go
@@ -1,7 +1,9 @@
 //go:generate httpstatusgen
 
-// Package httpstatus expands the simple Status and StatusCode values
-// returned by the net/http package to included classes, references
-// as well as an HTTPError that provides more useful information
-// from the http.Response object.
+/*
+Package httpstatus expands the simple Status and StatusCode values
+returned by the net/http package to included classes, references as well
+as an HTTPError that provides more useful information from the
+http.Response object.
+*/
 package httpstatus
