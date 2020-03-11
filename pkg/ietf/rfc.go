2d82e8076dd9ff9b76ca693e54e424b5711e9b34
Steve Moyer <smoyer@psu.edu>
2020-03-11 11:34:59 -0400

    Adds an object to provide a reference to an RFC

---
+++ pkg/ietf/rfc.go
@@ -0,0 +1,25 @@
+package rfc
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+// RFC represents a reference to an (adopted) IETF RFC
+type RFC struct {
+	Document string
+	Section  string
+}
+
+// URL returns the URL associated with the RFC Number and Section.
+func (rfc RFC) URL() *url.URL {
+	num, sec := normalize(rfc.Document), normalize(rfc.Section)
+	r := fmt.Sprintf("https://tools.ietf.org/html/%s#%s", num, sec)
+	u, _ := url.Parse(r)
+	return u
+}
+
+func normalize(s string) string {
+	return url.PathEscape(strings.ToLower(strings.ReplaceAll(s, "Section ", "Section-")))
+}
---
+++ pkg/ietf/rfc_test.go
@@ -0,0 +1,29 @@
+package rfc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestURL(t *testing.T) {
+	tests := []struct {
+		name string
+		inp  RFC
+		exp  string
+	}{
+		{"Valid", RFC{Document: "RFC1234", Section: "Section 6.4.1"}, "https://tools.ietf.org/html/rfc1234#section-6.4.1"},
+		{"Empty Number", RFC{Section: "Section 6.4.1"}, "https://tools.ietf.org/html/#section-6.4.1"},
+		{"Empty Section", RFC{Document: "RFC1234"}, "https://tools.ietf.org/html/rfc1234"},
+		{"Requires encoding", RFC{Document: "RFC1234", Section: "\"This is a test\""},
+			"https://tools.ietf.org/html/rfc1234#%22this%20is%20a%20test%22"},
+	}
+
+	for idx := range tests {
+		test := tests[idx]
+		t.Run(test.name, func(t *testing.T) {
+			act := test.inp.URL()
+			assert.Equal(t, test.exp, act.String())
+		})
+	}
+}
