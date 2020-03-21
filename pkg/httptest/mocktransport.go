a82379518fe335902e0dfde2f4accad14db1c5bd
Steve Moyer <smoyer@psu.edu>
2020-03-20 22:58:39 -0400

    Renames MockTransport to mirror DefaultTransport

--- pkg/httptest/mocktransport.go
+++ pkg/httptest/mocktransport.go
@@ -2,11 +2,11 @@ package httptest
 
 import "net/http"
 
-// MockRoundTripper implements the http.RoundTripper interface as noted
+// MockTransport implements the http.RoundTripper interface as noted
 // below but doesn't actually perform any network calls.  The Resp should
 // be set up before each HTTP call.  The Req is available for inspection
 // and/or assertions after the HTTP call.
-type MockRoundTripper struct {
+type MockTransport struct {
 	Req  *http.Request
 	Resp *http.Response
 	Err  error
@@ -14,7 +14,7 @@ type MockRoundTripper struct {
 
 // RoundTrip implements the http.RoundTripper interface.
 // https://pkg.go.dev/net/http?tab=doc#RoundTripper
-func (mrt MockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+func (mrt MockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	mrt.Req = req
 	return mrt.Resp, mrt.Err
 }
